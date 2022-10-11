cc325e6cda2027054515d6758ebebadd048a5e2d
Leonardo Vargas <leu1607@hotmail.com>
2022-10-11 20:19:06 -0300

    style: melhorias no card do jogo

--- src/components/Matchs/index.tsx
+++ src/components/Matchs/index.tsx
@@ -11,18 +11,23 @@ export const Matchs = () => (
     {matchs.map(match => (
       <article key={match.date} className={styles.match}>
         <header className={styles.match__header}>
-          <span>Grupo A</span>
-          <time>
-            <BsFillCalendarFill size={14} /> Dom 15/07 12h00
-          </time>
+          <div>
+            <span>Grupo A</span>
+            <time>
+              {/* <BsFillCalendarFill size={14} /> */}
+              Dom 15/07 12h00
+            </time>
+          </div>
+          <button className={styles.bet}>
+            <IoMdFootball />
+            PITACO
+          </button>
         </header>
 
         <div className={styles.match__content}>
           <CardTeam {...match.teamA} isFinished={match.status === 'finished'} />
           <CardTeam {...match.teamB} isFinished={match.status === 'finished'} />
         </div>
-
-        <button className={styles.bet}>Dar palpite</button>
       </article>
     ))}
   </section>
