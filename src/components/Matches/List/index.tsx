191d399547bcfa0600f0b4a386647019e42710c7
Leonardo Vargas <leu1607@hotmail.com>
2022-10-25 22:08:55 -0300

    style: ajustes na listagem do jogo

--- src/components/Matches/List/index.tsx
+++ src/components/Matches/List/index.tsx
@@ -34,13 +34,13 @@ export const ListMatches = ({ matches }: PageProps) => {
             <time className={styles.match__date}>
               {formatDate(match.date, 'normal')}
             </time>
-
-            {match.status === 'finished' && (
-              <span className={styles.status}>ENCERRADO</span>
-            )}
           </div>
 
           <div className={styles.match__content}>
+            {match.status === 'finished' && (
+              <span className={styles.status}>PARTIDA ENCERRADA</span>
+            )}
+
             <div className={styles.team}>
               <div className={styles.team__image}>
                 <Image
@@ -51,7 +51,7 @@ export const ListMatches = ({ matches }: PageProps) => {
               </div>
 
               <strong>{match.teamA.name}</strong>
-              <span>{match.isBet ? match.teamA.score : '-'}</span>
+              <span>{match.isBet ? match.teamA.score : '2'}</span>
             </div>
 
             <span className={styles.versus}>VS</span>
@@ -66,7 +66,7 @@ export const ListMatches = ({ matches }: PageProps) => {
               </div>
 
               <strong>{match.teamB.name}</strong>
-              <span>{match.isBet ? match.teamB.score : '-'}</span>
+              <span>{match.isBet ? match.teamB.score : '5'}</span>
             </div>
           </div>
 
@@ -89,17 +89,24 @@ export const ListMatches = ({ matches }: PageProps) => {
               </small>
             </>
           ) : (
-            <div className={styles.aaa}>
-              <p>
-                <strong>Resultado:</strong> {match.result.scoreTeamA} x{' '}
-                {match.result.scoreTeamB}
-              </p>
+            <div className={styles.match__resume}>
               {data?.user.bets[match._id] && (
-                <p>
-                  <strong>Total de pontos:</strong>{' '}
-                  {data?.user.bets[match._id].points}
+                <p
+                  className={styles.bet__points}
+                  title="Total de pontos marcados no palpite"
+                >
+                  <strong>{data?.user.bets[match._id].points}</strong>
+                  <small>pontos</small>
                 </p>
               )}
+
+              <div>
+                <strong>Resultado</strong>
+                <p>
+                  <span>{match.result.scoreTeamA}</span>x
+                  <span>{match.result.scoreTeamB}</span>
+                </p>
+              </div>
             </div>
           )}
         </article>
