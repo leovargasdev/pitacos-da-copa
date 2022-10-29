969fee172d85717447dd25b0bed397b59ddde389
Leonardo Vargas <leu1607@hotmail.com>
2022-10-29 11:04:38 -0300

    feat: criando um filtro com os tipos dos jogos

--- src/components/Matches/List/index.tsx
+++ src/components/Matches/List/index.tsx
@@ -11,9 +11,10 @@ import { MatchTeams } from './MatchTeams'
 import styles from './styles.module.scss'
 interface PageProps {
   matches: Match[]
+  seletedType: string
 }
 
-export const ListMatches = ({ matches }: PageProps) => {
+export const ListMatches = ({ matches, seletedType }: PageProps) => {
   const { handleOpenBet } = useBet()
   const { status, data } = useSession()
 
@@ -23,63 +24,68 @@ export const ListMatches = ({ matches }: PageProps) => {
 
   return (
     <section className={styles.matches}>
-      {matches.map(match => (
-        <article key={match._id} className={styles.match}>
-          <MatchInfo {...match} />
+      {matches.map(
+        match =>
+          (!seletedType || match.type === seletedType) && (
+            <article key={match._id} className={styles.match}>
+              <MatchInfo {...match} />
 
-          <MatchTeams {...match} />
+              <MatchTeams {...match} />
 
-          {match.status === 'active' && !isPast(new Date(match.date)) && (
-            <>
-              <button
-                type="button"
-                onClick={() => handleOpenBet(match)}
-                disabled={isDisableBet(match.date)}
-                className={styles.bet__button}
-              >
-                {match.isBet ? 'Atualizar' : 'Adicionar'} pitaco
-              </button>
+              {match.status === 'active' && !isPast(new Date(match.date)) && (
+                <>
+                  <button
+                    type="button"
+                    onClick={() => handleOpenBet(match)}
+                    disabled={isDisableBet(match.date)}
+                    className={styles.bet__button}
+                  >
+                    {match.isBet ? 'Atualizar' : 'Adicionar'} pitaco
+                  </button>
 
-              <button
-                type="button"
-                onClick={() => handleOpenBet(match)}
-                // disabled={isDisableBet(match.date)}
-                className={styles['bet__button-mobile']}
-              >
-                <BiMessageSquareAdd size={32} />
-              </button>
+                  <button
+                    type="button"
+                    onClick={() => handleOpenBet(match)}
+                    // disabled={isDisableBet(match.date)}
+                    className={styles['bet__button-mobile']}
+                  >
+                    <BiMessageSquareAdd size={32} />
+                  </button>
 
-              <small
-                aria-disabled={!match.isBet}
-                className={styles.bet__status}
-                title={match.isBet ? 'Pitaco salvo' : 'Ainda não fez o pitaco'}
-              >
-                <BsCheckCircleFill />
-              </small>
-            </>
-          )}
+                  <small
+                    aria-disabled={!match.isBet}
+                    className={styles.bet__status}
+                    title={
+                      match.isBet ? 'Pitaco salvo' : 'Ainda não fez o pitaco'
+                    }
+                  >
+                    <BsCheckCircleFill />
+                  </small>
+                </>
+              )}
 
-          {match.status === 'finished' && (
-            <div className={styles.match__resume}>
-              <p
-                className={styles.bet__points}
-                title="Total de pontos marcados no palpite"
-              >
-                <strong>{data?.user.bets[match._id]?.points || 0}</strong>
-                <small>pontos</small>
-              </p>
+              {match.status === 'finished' && (
+                <div className={styles.match__resume}>
+                  <p
+                    className={styles.bet__points}
+                    title="Total de pontos marcados no palpite"
+                  >
+                    <strong>{data?.user.bets[match._id]?.points || 0}</strong>
+                    <small>pontos</small>
+                  </p>
 
-              <div>
-                <strong>Resultado</strong>
-                <p>
-                  <span>{match.result.scoreTeamA}</span>x
-                  <span>{match.result.scoreTeamB}</span>
-                </p>
-              </div>
-            </div>
-          )}
-        </article>
-      ))}
+                  <div>
+                    <strong>Resultado</strong>
+                    <p>
+                      <span>{match.result.scoreTeamA}</span>x
+                      <span>{match.result.scoreTeamB}</span>
+                    </p>
+                  </div>
+                </div>
+              )}
+            </article>
+          )
+      )}
     </section>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,12 +8,15 @@ import { BetProvider } from 'hook/useBet'
 import { getMatches } from 'utils/format/match'
 import { GridMatches, ListMatches } from 'components/Matches'
 
+import styles from 'styles/home.module.scss'
+
 interface PageProps {
   matches: Match[]
 }
 
 const HomePage: NextPage<PageProps> = ({ matches: matchesDefault }) => {
   const { data } = useSession()
+  const [seletedType, setSeletedType] = useState<string>('')
   const [matches, setMatches] = useState(matchesDefault)
 
   useEffect(() => {
@@ -56,6 +59,10 @@ const HomePage: NextPage<PageProps> = ({ matches: matchesDefault }) => {
     )
   }
 
+  const onFilterType = (type: string): void => {
+    setSeletedType(state => (state === type ? '' : type))
+  }
+
   return (
     <BetProvider updateMatch={updateMatch}>
       <SEO
@@ -63,7 +70,32 @@ const HomePage: NextPage<PageProps> = ({ matches: matchesDefault }) => {
         title="Pitacos da copa 2022"
         description="Quer descobrir quem é o melhor palpiteiro entre os seus amigos, parentes ou colegas da firma? Faça login na nossa plataforma e comece a palpitar os jogos da copa do mundo de 2022"
       />
-      <ListMatches matches={matches} />
+      <div className={styles.filters}>
+        {[
+          'grupo-a',
+          'grupo-b',
+          'grupo-c',
+          'grupo-d',
+          'grupo-e',
+          'grupo-f',
+          'grupo-g',
+          'grupo-h'
+        ].map(type => (
+          <button
+            key={type}
+            type="button"
+            onClick={() => onFilterType(type)}
+            aria-label={`Botão para filtrar os jogos do tipo ${type.replace(
+              '-',
+              ' '
+            )}`}
+            aria-pressed={type === seletedType}
+          >
+            {type.replace('-', ' ')}
+          </button>
+        ))}
+      </div>
+      <ListMatches matches={matches} seletedType={seletedType} />
     </BetProvider>
   )
 }
