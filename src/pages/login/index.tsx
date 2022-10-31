811c9470b12fdd746afdccdf34fa9ec6d98c1cde
Leonardo Vargas <leu1607@hotmail.com>
2022-10-31 16:41:44 -0300

    feat: criando página de login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import { GetServerSideProps } from 'next'
-import { FaFacebook, FaGoogle } from 'react-icons/fa'
+import { FaFacebookSquare, FaGoogle } from 'react-icons/fa'
 import { signIn, getSession } from 'next-auth/react'
 
 import { SEO } from 'components/SEO'
@@ -9,29 +9,41 @@ import styles from './styles.module.scss'
 
 const LoginPage = () => (
   <div className={styles.container}>
-    <SEO tabName="Login" title="Entrar na plataforma" description="" />
+    <SEO
+      tabName="Acessar conta"
+      title="Faça login na plataforma pitacos da copa 2022"
+    />
 
-    <h1>Seja bem-vindo, faça o login para acessar a sua conta.</h1>
+    <figure className={styles.image}>
+      <Image
+        src="/login-background.jpg"
+        layout="fill"
+        objectFit="cover"
+        objectPosition="top"
+      />
+    </figure>
+
+    <h1>Seja bem-vindo, faça login para acessar a sua conta.</h1>
 
     <hr />
 
     <div className={styles.buttons}>
       <button
         type="button"
-        className={styles.github}
-        onClick={() => signIn('facebook')}
-      >
-        <FaFacebook />
-        Facebook
-      </button>
-
-      <button
-        type="button"
         className={styles.google}
         onClick={() => signIn('google')}
       >
         <FaGoogle />
-        Google
+        Entrar com Google
+      </button>
+
+      <button
+        type="button"
+        className={styles.facebook}
+        onClick={() => signIn('facebook')}
+      >
+        <FaFacebookSquare />
+        Entrar com Facebook
       </button>
     </div>
   </div>
